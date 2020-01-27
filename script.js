80b1cf39075900ca5fb8e1748c0c325586f27e0d
Reza Masoumpour <reza.mas88@gmail.com>
2020-01-27 21:18:10 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+bodyStyle = document.querySelector("body");
+btnResult = document.querySelector(".btnResult");
+
+function randomizeColor() {
+  let listArrNum = [48, 49, 50, 51, 52, 53, 54, 55,
+            56, 57, 65, 66, 67, 68, 69, 70];
+  let tempValue = "";
+  let finalValue = "";
+
+  for(let index = 0; index < 6; index++) {
+    tempValue += String.fromCharCode(listArrNum[Math.floor(Math.random() * listArrNum.length)]);
+  }
+  finalValue =  "#" + tempValue;
+  return finalValue;
+}
+
+btnResult.addEventListener("click", function(){
+  bodyStyle.style.background = randomizeColor();
+})
